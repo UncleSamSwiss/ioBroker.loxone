6b93d73fcf831a5affefe2364262d9e2c085c1bb
Samuel Weibel <samuel.weibel@gmail.com>
2020-09-24 10:36:48 +0200

    Added i18n files and regenerated words.js from them.

--- admin/words.js
+++ admin/words.js
@@ -2,21 +2,13 @@
 'use strict';
 
 systemDictionary = {
-    'Loxone Miniserver adapter settings': {
-        en: 'Loxone Miniserver adapter settings',
-        de: 'Loxone Miniserver Adapter-Einstellungen',
-        ru: 'Настройки драйвера Loxone Miniserver',
-    },
-    'Miniserver Hostname / IP': {
-        en: 'Miniserver Hostname / IP',
-        de: 'Hostname / IP des Miniservers',
-        ru: 'IP / Hostname Минисервера',
-    },
-    'Miniserver Port': { en: 'Miniserver Port', de: 'Port des Miniservers', ru: 'Порт Минисервера' },
-    'Miniserver Username': { en: 'Miniserver Username', de: 'Benutzername des Miniservers', ru: 'Имя пользователя' },
-    'Miniserver Password': { en: 'Miniserver Password', de: 'Kennwort des Miniservers', ru: 'Пароль' },
-    Synchronize: { en: 'Synchronize', de: 'Synchronisieren', ru: 'Синхронизация' },
-    Names: { en: 'Names', de: 'Namen', ru: 'имена' },
-    Rooms: { en: 'Rooms', de: 'Räume', ru: 'комнат' },
-    Functions: { en: 'Functions', de: 'Gewerke', ru: 'групп устройств' },
+    "Functions": {                                   "en": "Functions",                                       "de": "Gewerke",                                         "ru": "групп устройств"},
+    "Loxone Miniserver adapter settings": {          "en": "Loxone Miniserver adapter settings",              "de": "Loxone Miniserver Adapter-Einstellungen",         "ru": "Настройки драйвера Loxone Miniserver"},
+    "Miniserver Hostname / IP": {                    "en": "Miniserver Hostname / IP",                        "de": "Hostname / IP des Miniservers",                   "ru": "IP / Hostname Минисервера"},
+    "Miniserver Password": {                         "en": "Miniserver Password",                             "de": "Kennwort des Miniservers",                        "ru": "Пароль"},
+    "Miniserver Port": {                             "en": "Miniserver Port",                                 "de": "Port des Miniservers",                            "ru": "Порт Минисервера"},
+    "Miniserver Username": {                         "en": "Miniserver Username",                             "de": "Benutzername des Miniservers",                    "ru": "Имя пользователя"},
+    "Names": {                                       "en": "Names",                                           "de": "Namen",                                           "ru": "имена"},
+    "Rooms": {                                       "en": "Rooms",                                           "de": "Räume",                                           "ru": "комнат"},
+    "Synchronize": {                                 "en": "Synchronize",                                     "de": "Synchronisieren",                                 "ru": "Синхронизация"},
 };
