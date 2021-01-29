c075b36e2458434f8c2cebd65b7fbd34c5604dd7
Robin Rainton <robin@rainton.com>
2021-01-29 19:06:31 +0100

    #22 Added more R/W states

--- build/controls/IRoomControllerV2.js
+++ build/controls/IRoomControllerV2.js
@@ -8,7 +8,6 @@ class IRoomControllerV2 extends control_base_1.ControlBase {
         this.uuid = '';
     }
     async loadAsync(type, uuid, control) {
-        this.uuid = uuid;
         await this.updateObjectAsync(uuid, {
             type: type,
             common: {
@@ -17,19 +16,88 @@ class IRoomControllerV2 extends control_base_1.ControlBase {
             },
             native: { control: control },
         });
-        await this.loadOtherControlStatesAsync(control.name, uuid, control.states, ['comfortTolerance']);
+        await this.loadOtherControlStatesAsync(control.name, uuid, control.states, [
+            'comfortTemperature',
+            'comfortTolerance',
+            'absentMinOffset',
+            'absentMaxOffset',
+            'comfortTemperatureOffset',
+        ]);
+        await this.updateStateObjectAsync(uuid + '.comfortTemperature', {
+            name: control.name + ': comfortTemperature',
+            read: true,
+            write: true,
+            type: 'number',
+            role: 'level.temperature',
+        }, control.states.comfortTemperature, async (name, value) => {
+            await this.setStateAck(name, value);
+        });
+        this.addStateChangeListener(uuid + '.comfortTemperature', (oldValue, newValue) => {
+            this.sendCommand(control.uuidAction, 'setComfortTemperature/' + newValue);
+        });
         await this.updateStateObjectAsync(uuid + '.comfortTolerance', {
             name: control.name + ': comfortTolerance',
             read: true,
             write: true,
             type: 'number',
+            min: 0.5,
+            max: 3.0,
             role: 'level.temperature',
         }, control.states.comfortTolerance, async (name, value) => {
-            await this.setStateAck(this.uuid + '.' + name, value);
+            await this.setStateAck(name, value);
         });
         this.addStateChangeListener(uuid + '.comfortTolerance', (oldValue, newValue) => {
             this.sendCommand(control.uuidAction, 'setComfortTolerance/' + newValue);
         });
+        await this.updateStateObjectAsync(uuid + '.absentMinOffset', {
+            name: control.name + ': absentMinOffset',
+            read: true,
+            write: true,
+            type: 'number',
+            role: 'level.temperature',
+        }, control.states.absentMinOffset, async (name, value) => {
+            await this.setStateAck(name, value);
+        });
+        this.addStateChangeListener(uuid + '.absentMinOffset', (oldValue, newValue) => {
+            this.sendCommand(control.uuidAction, 'setAbsentMinTemperature/' + newValue);
+        });
+        await this.updateStateObjectAsync(uuid + '.absentMaxOffset', {
+            name: control.name + ': absentMaxOffset',
+            read: true,
+            write: true,
+            type: 'number',
+            role: 'level.temperature',
+        }, control.states.absentMaxOffset, async (name, value) => {
+            await this.setStateAck(name, value);
+        });
+        this.addStateChangeListener(uuid + '.absentMaxOffset', (oldValue, newValue) => {
+            this.sendCommand(control.uuidAction, 'setAbsentMaxTemperature/' + newValue);
+        });
+        await this.updateStateObjectAsync(uuid + '.comfortTemperatureOffset', {
+            name: control.name + ': comfortTemperatureOffset',
+            read: true,
+            write: true,
+            type: 'number',
+            role: 'level.temperature',
+        }, control.states.comfortTemperatureOffset, async (name, value) => {
+            await this.setStateAck(name, value);
+        });
+        this.addStateChangeListener(uuid + '.comfortTemperatureOffset', (oldValue, newValue) => {
+            this.sendCommand(control.uuidAction, 'setComfortModeTemp/' + newValue);
+        });
+        await this.createButtonCommandStateObjectAsync(control.name, uuid, 'stopOverride');
+        this.addStateChangeListener(uuid + '.stopOverride', () => {
+            this.sendCommand(control.uuidAction, 'stopOverride');
+        });
+        // TODO: Controls not yet implemented:
+        // override
+        // setManualTemperature
+        // setOperatingMode
+        // setComfortModeTemp
+        // set
+        // modeslist
+        // TODO: Subcontrols not yet implemented:
+        // IRCV2Daytimer
     }
 }
 exports.IRoomControllerV2 = IRoomControllerV2;
--- src/controls/IRoomControllerV2.ts
+++ src/controls/IRoomControllerV2.ts
@@ -6,7 +6,6 @@ export class IRoomControllerV2 extends ControlBase {
     private uuid = '';
 
     async loadAsync(type: ControlType, uuid: string, control: Control): Promise<void> {
-        this.uuid = uuid;
         await this.updateObjectAsync(uuid, {
             type: type,
             common: {
@@ -16,7 +15,34 @@ export class IRoomControllerV2 extends ControlBase {
             native: { control: control as any },
         });
 
-        await this.loadOtherControlStatesAsync(control.name, uuid, control.states, ['comfortTolerance']);
+        await this.loadOtherControlStatesAsync(control.name, uuid, control.states, [
+            'comfortTemperature',
+            'comfortTolerance',
+            'absentMinOffset',
+            'absentMaxOffset',
+            'comfortTemperatureOffset',
+        ]);
+
+        await this.updateStateObjectAsync(
+            uuid + '.comfortTemperature',
+            {
+                name: control.name + ': comfortTemperature',
+                read: true,
+                write: true,
+                type: 'number',
+                role: 'level.temperature',
+            },
+            control.states.comfortTemperature,
+            async (name: string, value: any) => {
+                await this.setStateAck(name, value);
+            },
+        );
+        this.addStateChangeListener(
+            uuid + '.comfortTemperature',
+            (oldValue: OldStateValue, newValue: CurrentStateValue) => {
+                this.sendCommand(control.uuidAction, 'setComfortTemperature/' + newValue);
+            },
+        );
 
         await this.updateStateObjectAsync(
             uuid + '.comfortTolerance',
@@ -25,11 +51,13 @@ export class IRoomControllerV2 extends ControlBase {
                 read: true,
                 write: true,
                 type: 'number',
+                min: 0.5,
+                max: 3.0,
                 role: 'level.temperature',
             },
             control.states.comfortTolerance,
             async (name: string, value: any) => {
-                await this.setStateAck(this.uuid + '.' + name, value);
+                await this.setStateAck(name, value);
             },
         );
         this.addStateChangeListener(
@@ -38,5 +66,84 @@ export class IRoomControllerV2 extends ControlBase {
                 this.sendCommand(control.uuidAction, 'setComfortTolerance/' + newValue);
             },
         );
+
+        await this.updateStateObjectAsync(
+            uuid + '.absentMinOffset',
+            {
+                name: control.name + ': absentMinOffset',
+                read: true,
+                write: true,
+                type: 'number',
+                role: 'level.temperature',
+            },
+            control.states.absentMinOffset,
+            async (name: string, value: any) => {
+                await this.setStateAck(name, value);
+            },
+        );
+        this.addStateChangeListener(
+            uuid + '.absentMinOffset',
+            (oldValue: OldStateValue, newValue: CurrentStateValue) => {
+                this.sendCommand(control.uuidAction, 'setAbsentMinTemperature/' + newValue);
+            },
+        );
+
+        await this.updateStateObjectAsync(
+            uuid + '.absentMaxOffset',
+            {
+                name: control.name + ': absentMaxOffset',
+                read: true,
+                write: true,
+                type: 'number',
+                role: 'level.temperature',
+            },
+            control.states.absentMaxOffset,
+            async (name: string, value: any) => {
+                await this.setStateAck(name, value);
+            },
+        );
+        this.addStateChangeListener(
+            uuid + '.absentMaxOffset',
+            (oldValue: OldStateValue, newValue: CurrentStateValue) => {
+                this.sendCommand(control.uuidAction, 'setAbsentMaxTemperature/' + newValue);
+            },
+        );
+
+        await this.updateStateObjectAsync(
+            uuid + '.comfortTemperatureOffset',
+            {
+                name: control.name + ': comfortTemperatureOffset',
+                read: true,
+                write: true,
+                type: 'number',
+                role: 'level.temperature',
+            },
+            control.states.comfortTemperatureOffset,
+            async (name: string, value: any) => {
+                await this.setStateAck(name, value);
+            },
+        );
+        this.addStateChangeListener(
+            uuid + '.comfortTemperatureOffset',
+            (oldValue: OldStateValue, newValue: CurrentStateValue) => {
+                this.sendCommand(control.uuidAction, 'setComfortModeTemp/' + newValue);
+            },
+        );
+
+        await this.createButtonCommandStateObjectAsync(control.name, uuid, 'stopOverride');
+        this.addStateChangeListener(uuid + '.stopOverride', () => {
+            this.sendCommand(control.uuidAction, 'stopOverride');
+        });
+
+        // TODO: Controls not yet implemented:
+        // override
+        // setManualTemperature
+        // setOperatingMode
+        // setComfortModeTemp
+        // set
+        // modeslist
+
+        // TODO: Subcontrols not yet implemented:
+        // IRCV2Daytimer
     }
 }
