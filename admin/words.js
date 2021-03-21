7e1a15f4687bb8bd61f7e31b9f1a0420aff92ade
Dutchman <rdrozda@hotmail.com>
2021-03-21 00:02:20 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: ioBroker Adapters/loxone
    Translate-URL: https://weblate.iobroker.net/projects/adapters/loxone/nl/

--- admin/words.js
+++ admin/words.js
@@ -2,13 +2,13 @@
 'use strict';
 
 systemDictionary = {
-    "Functions": {                                   "en": "Functions",                                       "de": "Gewerke",                                         "ru": "групп устройств"},
-    "Loxone Miniserver adapter settings": {          "en": "Loxone Miniserver adapter settings",              "de": "Loxone Miniserver Adapter-Einstellungen",         "ru": "Настройки драйвера Loxone Miniserver"},
-    "Miniserver Hostname / IP": {                    "en": "Miniserver host name / IP",                       "de": "Hostname / IP des Miniservers",                   "ru": "IP / Hostname Минисервера"},
-    "Miniserver Password": {                         "en": "Miniserver password",                             "de": "Kennwort des Miniservers",                        "ru": "Пароль"},
-    "Miniserver Port": {                             "en": "Miniserver port",                                 "de": "Port des Miniservers",                            "ru": "Порт Минисервера"},
-    "Miniserver Username": {                         "en": "Miniserver username",                             "de": "Benutzername des Miniservers",                    "ru": "Имя пользователя"},
-    "Names": {                                       "en": "Names",                                           "de": "Namen",                                           "ru": "имена"},
-    "Rooms": {                                       "en": "Rooms",                                           "de": "Räume",                                           "ru": "комнат"},
-    "Synchronize": {                                 "en": "Synchronize",                                     "de": "Synchronisieren",                                 "ru": "Синхронизация"},
+    "Functions": {                                   "en": "Functions",                                       "de": "Gewerke",                                         "ru": "групп устройств",                                 "nl": "Functies"},
+    "Loxone Miniserver adapter settings": {          "en": "Loxone Miniserver adapter settings",              "de": "Loxone Miniserver Adapter-Einstellungen",         "ru": "Настройки драйвера Loxone Miniserver",            "nl": "Loxone Miniserver adapter instellingen"},
+    "Miniserver Hostname / IP": {                    "en": "Miniserver host name / IP",                       "de": "Hostname / IP des Miniservers",                   "ru": "IP / Hostname Минисервера",                       "nl": "Miniserver hostnaam / IP"},
+    "Miniserver Password": {                         "en": "Miniserver password",                             "de": "Kennwort des Miniservers",                        "ru": "Пароль",                                          "nl": "Miniserver wachtwoord"},
+    "Miniserver Port": {                             "en": "Miniserver port",                                 "de": "Port des Miniservers",                            "ru": "Порт Минисервера",                                "nl": "Miniserver poort"},
+    "Miniserver Username": {                         "en": "Miniserver username",                             "de": "Benutzername des Miniservers",                    "ru": "Имя пользователя",                                "nl": "Miniserver gebruikersnaam"},
+    "Names": {                                       "en": "Names",                                           "de": "Namen",                                           "ru": "имена",                                           "nl": "Namen"},
+    "Rooms": {                                       "en": "Rooms",                                           "de": "Räume",                                           "ru": "комнат",                                          "nl": "Ruimtes"},
+    "Synchronize": {                                 "en": "Synchronize",                                     "de": "Synchronisieren",                                 "ru": "Синхронизация",                                   "nl": "Syncroniseer"},
 };
