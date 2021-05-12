b9ca57ddd6c890eb953dad81b638b814bd0c1097
Samuel Weibel <samuel.weibel@gmail.com>
2021-05-12 08:36:19 +0200

    Implemented ValueSelector (fixes #36)

---
+++ src/controls/ValueSelector.ts
@@ -0,0 +1,29 @@
+import { CurrentStateValue, OldStateValue } from '../main';
+import { Control } from '../structure-file';
+import { ControlBase, ControlType } from './control-base';
+
+export class ValueSelector extends ControlBase {
+    async loadAsync(type: ControlType, uuid: string, control: Control): Promise<void> {
+        await this.updateObjectAsync(uuid, {
+            type: type,
+            common: {
+                name: control.name,
+                role: 'sensor',
+            },
+            native: { control: control as any },
+        });
+
+        await this.loadOtherControlStatesAsync(control.name, uuid, control.states, ['value', 'min', 'max', 'step']);
+
+        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'value', 'number', 'level', {
+            write: true,
+        });
+        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'min', 'number', 'value');
+        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'max', 'number', 'value');
+        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'step', 'number', 'value');
+
+        this.addStateChangeListener(uuid + '.value', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
+            this.sendCommand(control.uuidAction, this.convertStateToInt(newValue).toString());
+        });
+    }
+}
