d4a48d59ff17f9fa56082c89a31a83b7a7cf658f
Fullstack0verflow <fullstackoverflow@gmx.de>
2021-12-24 11:15:30 +0100

    test

--- build/controls/AudioZoneV2.js
+++ build/controls/AudioZoneV2.js
@@ -18,20 +18,7 @@ class AudioZoneV2 extends control_base_1.ControlBase {
             'clientState',
             'power',
             'volume',
-            'maxVolume',
             'volumeStep',
-            'shuffle',
-            'sourceList',
-            'repeat',
-            'songName',
-            'duration',
-            'progress',
-            'album',
-            'artist',
-            'station',
-            'genre',
-            'cover',
-            'source',
         ]);
         const serverStates = {
             '-3': 'invalid zone',
@@ -60,21 +47,6 @@ class AudioZoneV2 extends control_base_1.ControlBase {
             // TODO: re-add: smartIgnore: false,
         });
         await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'volume', 'number', 'level.volume', { write: true });
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'maxVolume', 'number', 'value');
-        await this.createBooleanControlStateObjectAsync(control.name, uuid, control.states, 'shuffle', 'switch', {
-            write: true,
-        });
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'sourceList', 'string', 'json');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'repeat', 'number', 'value', { write: true });
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'songName', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'duration', 'number', 'value.interval');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'progress', 'number', 'value.interval', { write: true });
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'album', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'artist', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'station', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'genre', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'cover', 'string', 'text.url');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'source', 'number', 'value', { write: true });
         this.addStateChangeListener(uuid + '.playState', (oldValue, newValue) => {
             newValue = this.convertStateToInt(newValue);
             if (newValue === 0 || newValue === 1) {
@@ -89,18 +61,6 @@ class AudioZoneV2 extends control_base_1.ControlBase {
         });
         this.addStateChangeListener(uuid + '.volume', (oldValue, newValue) => {
             this.sendCommand(control.uuidAction, 'volume/' + newValue);
-        });
-        this.addStateChangeListener(uuid + '.shuffle', (oldValue, newValue) => {
-            this.sendCommand(control.uuidAction, 'shuffle/' + (newValue ? 1 : 0));
-        });
-        this.addStateChangeListener(uuid + '.repeat', (oldValue, newValue) => {
-            this.sendCommand(control.uuidAction, 'repeat/' + newValue);
-        });
-        this.addStateChangeListener(uuid + '.progress', (oldValue, newValue) => {
-            this.sendCommand(control.uuidAction, 'progress/' + newValue);
-        });
-        this.addStateChangeListener(uuid + '.source', (oldValue, newValue) => {
-            this.sendCommand(control.uuidAction, 'source/' + newValue);
         });
         await this.createButtonCommandStateObjectAsync(control.name, uuid, 'prev');
         this.addStateChangeListener(uuid + '.prev', () => {
--- src/controls/AudioZoneV2.ts
+++ src/controls/AudioZoneV2.ts
@@ -19,20 +19,7 @@ export class AudioZoneV2 extends ControlBase {
             'clientState',
             'power',
             'volume',
-            'maxVolume',
             'volumeStep',
-            'shuffle',
-            'sourceList',
-            'repeat',
-            'songName',
-            'duration',
-            'progress',
-            'album',
-            'artist',
-            'station',
-            'genre',
-            'cover',
-            'source',
         ]);
 
         const serverStates = {
@@ -94,81 +81,6 @@ export class AudioZoneV2 extends ControlBase {
             'level.volume',
             { write: true },
         );
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'maxVolume',
-            'number',
-            'value',
-        );
-        await this.createBooleanControlStateObjectAsync(control.name, uuid, control.states, 'shuffle', 'switch', {
-            write: true,
-        });
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'sourceList',
-            'string',
-            'json',
-        );
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'repeat',
-            'number',
-            'value',
-            { write: true },
-        );
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'songName',
-            'string',
-            'text',
-        );
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'duration',
-            'number',
-            'value.interval',
-        );
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'progress',
-            'number',
-            'value.interval',
-            { write: true },
-        );
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'album', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'artist', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'station', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(control.name, uuid, control.states, 'genre', 'string', 'text');
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'cover',
-            'string',
-            'text.url',
-        );
-        await this.createSimpleControlStateObjectAsync(
-            control.name,
-            uuid,
-            control.states,
-            'source',
-            'number',
-            'value',
-            { write: true },
-        );
-
         this.addStateChangeListener(uuid + '.playState', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
             newValue = this.convertStateToInt(newValue);
             if (newValue === 0 || newValue === 1) {
@@ -183,19 +95,6 @@ export class AudioZoneV2 extends ControlBase {
         this.addStateChangeListener(uuid + '.volume', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
             this.sendCommand(control.uuidAction, 'volume/' + newValue);
         });
-        this.addStateChangeListener(uuid + '.shuffle', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
-            this.sendCommand(control.uuidAction, 'shuffle/' + (newValue ? 1 : 0));
-        });
-        this.addStateChangeListener(uuid + '.repeat', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
-            this.sendCommand(control.uuidAction, 'repeat/' + newValue);
-        });
-        this.addStateChangeListener(uuid + '.progress', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
-            this.sendCommand(control.uuidAction, 'progress/' + newValue);
-        });
-        this.addStateChangeListener(uuid + '.source', (oldValue: OldStateValue, newValue: CurrentStateValue) => {
-            this.sendCommand(control.uuidAction, 'source/' + newValue);
-        });
-
         await this.createButtonCommandStateObjectAsync(control.name, uuid, 'prev');
         this.addStateChangeListener(uuid + '.prev', () => {
             this.sendCommand(control.uuidAction, 'prev');
